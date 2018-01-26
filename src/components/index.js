3f76e2461952e5bd894273d297091c182c9ea947
Robin Luongo <robin.luongo@gmail.com>
2018-01-26 18:30:37 -0500

    removing useless main component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,1 @@
 export { default as DatabaseSelect } from './databaseSelect'
-export {default as Main} from './main'
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Route, Switch, Router, withRouter } from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { DatabaseSelect, Main } from './components'
+import { DatabaseSelect } from './components'
 
 import createHashHistory from 'history/createHashHistory'
 
@@ -18,11 +18,9 @@ export default class Routes extends Component {
     return (
       <MuiThemeProvider>
         <Router history={newHistory}>
-          <Main>
             <Switch>
               <Route path="/" component={DatabaseSelect} />
             </Switch>
-          </Main>
         </Router>
       </MuiThemeProvider>
     )
