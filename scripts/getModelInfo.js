f18307aeff879dcc4965f249e674b930944204e1
Josh Luria <josh.luria@gmail.com>
2018-01-30 17:22:24 -0500

    Change naming convention to reflect models and attributes

--- scripts/getModelInfo.js
+++ scripts/getModelInfo.js
@@ -1,4 +1,4 @@
-import {fromJS} from 'immutable'
+const {fromJS} = require('immutable')
 
 async function getModelInfo(selectedDbName) {
   const { Client } = require('pg')
@@ -10,56 +10,55 @@ async function getModelInfo(selectedDbName) {
 
   await client.connect()
 
-  const res = await client.query("SELECT * FROM pg_catalog.pg_tables WHERE schemaname = 'public'")
+  // Get all models for the selected database
+  const res = await client.query(`SELECT *
+                                  FROM pg_catalog.pg_tables
+                                  WHERE schemaname = 'public'`)
 
+  const modelNames = res.rows.map(row => row.tablename)
 
-  const tableNames = res.rows.map(row => row.tablename)
-
-  const promisedDB = buildTableObjects(tableNames, client)
+  const promisedDB = buildModelObjects(modelNames, client)
 
   return Promise.all(promisedDB).then(db => fromJS(db))
 }
 
-function buildTableObjects(tableNames, client) {
-  return tableNames.map(async (table, idx) => {
-    const tableObject = {
+function buildModelObjects(modelNames, client) {
+  return modelNames.map(async (model, idx) => {
+    const modelObject = {
       key: idx,
-      name: table,
+      name: model,
       attributes: []
     }
 
-    const attributes = await client.query(`SELECT column_name, ordinal_position, data_type from information_schema.columns WHERE table_name = '${table}'`)
+    // Get each attribute on a model, their index, and data type
+    const attributes = await client.query(`SELECT column_name, ordinal_position, data_type
+                                           FROM information_schema.columns
+                                           WHERE table_name = '${model}'`)
 
-
-    const tableAttributeList = buildTableAttributeList(attributes)
-    tableObject.attributes = tableAttributeList
-    return tableObject
+    const modelAttributeList = buildModelAttributeList(attributes)
+    modelObject.attributes = modelAttributeList
+    return modelObject
   })
 }
 
-function buildTableAttributeList(attributes) {
+function buildModelAttributeList(attributes) {
+  const dataTypeMapping = { 'character varying': 'STRING',
+                            'integer': 'INTEGER',
+                            'timestamp with time zone': 'DATE'
+                          }
+
   return attributes.rows.map(attribute => {
-    let dataType;
-    switch (attribute.data_type) {
-      case 'character varying':
-        dataType = 'STRING'
-        break;
-      case 'integer':
-        dataType = 'INTEGER'
-        break;
-      case 'timestamp with time zone':
-        dataType = 'DATE'
-        break;
-      default:
-        dataType = attribute.data_type;
-    }
+    //attribute.datatype = dataTypeMapping[attribute.datatype] || attribute.datatype
 
     return {
       key: attribute.ordinal_position,
       name: attribute.column_name,
-      type: dataType
+      type: dataTypeMapping[attribute.dataType] || attribute.datatype
     }
   })
 }
 
-export default getModelinfo
+const dbName = 'warbler_testing'
+getModelInfo(dbName).then(res => console.log(res))
+
+//export default getModelinfo
