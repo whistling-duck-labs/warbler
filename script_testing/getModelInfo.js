91c5426df95324cdea1e7066f058f0c7bc540b50
Josh Luria <josh.luria@gmail.com>
2018-01-30 14:18:39 -0500

    Refactor get model info script

--- script_testing/getModelInfo.js
+++ script_testing/getModelInfo.js
@@ -1,11 +1,11 @@
 import {fromJS} from 'immutable'
 
-async function getModelinfo(dbName) {
+async function getModelInfo(selectedDbName) {
   const { Client } = require('pg')
 
   // We don't need username and password as long as password is null
   const portSetting = 'postgres://localhost:5432/'
-  const postgresUrl = portSetting + dbName
+  const postgresUrl = portSetting + selectedDbName
   const client = new Client(postgresUrl)
 
   await client.connect()
@@ -15,7 +15,13 @@ async function getModelinfo(dbName) {
 
   const tableNames = res.rows.map(row => row.tablename)
 
-  const promisedDB = tableNames.map(async (table, idx) => {
+  const promisedDB = buildTableObjects(tableNames, client)
+
+  return Promise.all(promisedDB).then(db => fromJS(db))
+}
+
+function buildTableObjects(tableNames, client) {
+  return tableNames.map(async (table, idx) => {
     const tableObject = {
       key: idx,
       name: table,
@@ -25,33 +31,35 @@ async function getModelinfo(dbName) {
     const attributes = await client.query(`SELECT column_name, ordinal_position, data_type from information_schema.columns WHERE table_name = '${table}'`)
 
 
-    const attributesList = attributes.rows.map(attribute => {
-      let dataType;
-      switch (attribute.data_type) {
-        case 'character varying':
-          dataType = 'STRING'
-          break;
-        case 'integer':
-          dataType = 'INTEGER'
-          break;
-        case 'timestamp with time zone':
-          dataType = 'DATE'
-          break;
-        default:
-          dataType = attribute.data_type;
-      }
-
-      return {
-        key: attribute.ordinal_position,
-        name: attribute.column_name,
-        type: dataType
-      }
-    })
-    tableObject.attributes = attributesList
+    const tableAttributeList = buildTableAttributeList(attributes)
+    tableObject.attributes = tableAttributeList
     return tableObject
   })
+}
 
-  return Promise.all(promisedDB).then(db => fromJS(db))
+function buildTableAttributeList(attributes) {
+  return attributes.rows.map(attribute => {
+    let dataType;
+    switch (attribute.data_type) {
+      case 'character varying':
+        dataType = 'STRING'
+        break;
+      case 'integer':
+        dataType = 'INTEGER'
+        break;
+      case 'timestamp with time zone':
+        dataType = 'DATE'
+        break;
+      default:
+        dataType = attribute.data_type;
+    }
+
+    return {
+      key: attribute.ordinal_position,
+      name: attribute.column_name,
+      type: dataType
+    }
+  })
 }
 
 export default getModelinfo
