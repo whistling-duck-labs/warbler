ff77f62894ed1aef9ff2212ae3278594767579f6
Josh Luria <josh.luria@gmail.com>
2018-02-06 15:40:38 -0500

    Lift project directory path to the store from project prompt component

--- src/components/projectPrompt.js
+++ src/components/projectPrompt.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Dialog, RaisedButton, Checkbox, Divider } from 'material-ui'
 import {Toolbar, ToolbarGroup, ToolbarSeparator} from 'material-ui/Toolbar'
+import {setProjectDirPath} from '../store/project'
 import shell from 'shelljs'
 const remote = require('electron').remote
 
-export default class ProjectPrompt extends Component {
+class ProjectPrompt extends Component {
   constructor (props) {
     super(props)
     this.state = {
       open: false,
-      directory: ''
     }
 
     this.handleClose = this.handleClose.bind(this)
@@ -31,20 +32,22 @@ export default class ProjectPrompt extends Component {
 
   openDirectory () {
     remote.dialog.showOpenDialog({properties: ['openDirectory']}, (directory) => {
-      this.setState({directory})
+      console.log(directory[0])
+      this.props.setProjectDirPath(directory[0])
       this.handleClose()
     })
   }
 
   createDirectory () {
     remote.dialog.showSaveDialog((directory) => {
-      this.setState({directory})
-      shell.mkdir(this.state.directory)
+      this.props.setProjectDirPath(directory[0])
+      shell.mkdir(this.props.projectDirPath)
       this.handleClose()
     })
   }
 
   render() {
+      console.log(this.props)
     const actions = (
       <div className='project-prompt-action'>
         <div className='folder' onClick={this.openDirectory}>
@@ -79,3 +82,11 @@ export default class ProjectPrompt extends Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  projectDirPath: state.get('projectDirPath')
+})
+
+const mapDispatchToProps = {setProjectDirPath}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectPrompt)
