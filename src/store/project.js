d4189c721321c259fab84ca6b6c3a01a109ce00a
Josh Luria <josh.luria@gmail.com>
2018-02-06 15:39:19 -0500

    Remove unnecessary code from project reducer

--- src/store/project.js
+++ src/store/project.js
@@ -1,10 +1,8 @@
-import {Map} from 'immutable'
-import toastr from 'toastr'
 // actions
-export const ADD_PROJECT_DIR_PATH = 'ADD_PROJECT_DIR_PATH'
+const SET_PROJECT_DIR_PATH = 'SET_PROJECT_DIR_PATH'
 
 // action creator that sets project path
-const setProjectDirPath = projectDirPath => ({type: INIT_DB, projectDirPath})
+export const setProjectDirPath = projectDirPath => ({type: SET_PROJECT_DIR_PATH, projectDirPath})
 
 // initial state
 
@@ -14,7 +12,7 @@ const initialProjectDirPath = ''
 
 export default function (state = initialProjectDirPath, action) {
   switch (action.type) {
-    case ADD_PROJECT_DIR_PATH:
+    case SET_PROJECT_DIR_PATH:
       return action.projectDirPath
     default:
       return state
